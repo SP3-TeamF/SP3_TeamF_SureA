e045e4f7ea240bac5980656a857e5906a148968d
Glence <glencelow@hotmail.com>
2016-08-15 15:58:56 +0800

    Controller crash -Ashley

--- SP-3 Framework/Base/Source/ControllerInput.cpp
+++ SP-3 Framework/Base/Source/ControllerInput.cpp
@@ -24,77 +24,120 @@ void ControllerInput::ControllerInput_Init(CONTROLLER_ID controllerID)
 
 bool ControllerInput::GetIsKeyPressed(CONTROLLER_INPUT_BUTTONS CONTROLLER_BUTTON)
 {
-	int controllerInput;
-	const unsigned char * arrayButtonStatus = glfwGetJoystickButtons(0, &controllerInput);
+	if (glfwJoystickPresent(controllerID))
+	{
+		int controllerInput;
+		const unsigned char * arrayButtonStatus = glfwGetJoystickButtons(0, &controllerInput);
 
-	return (arrayButtonStatus[CONTROLLER_BUTTON] == GLFW_PRESS);
+		return (arrayButtonStatus[CONTROLLER_BUTTON] == GLFW_PRESS);
+	}
+
+	return false;
 }
 
 bool ControllerInput::GetIsKeyHeld(CONTROLLER_INPUT_BUTTONS CONTROLLER_BUTTON)
 {
-	int controllerInput;
-	const unsigned char * arrayButtonStatus = glfwGetJoystickButtons(0, &controllerInput);
+	if (glfwJoystickPresent(controllerID))
+	{
+		int controllerInput;
+		const unsigned char * arrayButtonStatus = glfwGetJoystickButtons(0, &controllerInput);
 
-	return (arrayButtonStatus[CONTROLLER_BUTTON] == GLFW_REPEAT);
+		return (arrayButtonStatus[CONTROLLER_BUTTON] == GLFW_REPEAT);
+	}
+
+	return false;
 }
 
 bool ControllerInput::GetIsKeyReleased(CONTROLLER_INPUT_BUTTONS CONTROLLER_BUTTON)
 {
-	int controllerInput;
-	const unsigned char * arrayButtonStatus = glfwGetJoystickButtons(0, &controllerInput);
+	if (glfwJoystickPresent(controllerID))
+	{
+		int controllerInput;
+		const unsigned char * arrayButtonStatus = glfwGetJoystickButtons(0, &controllerInput);
 
-	return (arrayButtonStatus[CONTROLLER_BUTTON] == GLFW_RELEASE);
+		return (arrayButtonStatus[CONTROLLER_BUTTON] == GLFW_RELEASE);
+	}
+	return false;
 }
 
 bool ControllerInput::GetIsTriggerPressed(CONTROLLER_TRIGGER TRIGGER_TYPE)
 {
-	int JoystickData;
-	const float* storedTrigger = glfwGetJoystickAxes(controllerID, &JoystickData);
-
-	switch (TRIGGER_TYPE)
+	if (glfwJoystickPresent(controllerID))
 	{
-		case L_TRIGGER :
+		int JoystickData;
+		const float* storedTrigger = glfwGetJoystickAxes(controllerID, &JoystickData);
+
+		switch (TRIGGER_TYPE)
 		{
-			if (storedTrigger[2] > 0.5)
+			case L_TRIGGER:
 			{
-				return true;
+				if (storedTrigger[2] > 0.5)
+				{
+					return true;
+				}
+				break;
 			}
-			break;
+			case R_TRIGGER:
+			{
+				if (storedTrigger[2] < -0.5)
+				{
+					return true;
+				}
+				break;
+			}
+			default:
+				break;
 		}
-		case R_TRIGGER:
-		{
-			if (storedTrigger[2] < -0.5)
-			{
-				return true;
-			}
-			break; 
-		}	
-		default:
-			break;
 	}
 	return false;
 }
 
 Vector3 ControllerInput::GetDirection(CONTROLLER_JOYSTICK JOYSTICK_TYPE)
 {
-    int JoystickData;
-	const float* storedAxes = glfwGetJoystickAxes(controllerID, &JoystickData);
+	float dirX = 0;
+	float dirY = 0;
 
-    switch (JOYSTICK_TYPE)
-        {
-        case L_JOYSTICK:
-        {
-			return Vector3(storedAxes[0], storedAxes[1]);
-            break;
-        }
-        case R_JOYSTICK:
-        {
-			return Vector3(storedAxes[3], storedAxes[4]);
-            break;
-        }
-        default:
-        {
-            break;
-        }
-    }
+	if (glfwJoystickPresent(controllerID))
+	{
+		int JoystickData;
+		const float* storedAxes = glfwGetJoystickAxes(controllerID, &JoystickData);
+
+		switch (JOYSTICK_TYPE)
+		{
+			case L_JOYSTICK:
+			{
+				dirX = storedAxes[0];
+				dirY = storedAxes[1];
+				if (dirX > -0.1 && dirX < 0.1)
+				{
+					dirX = 0;
+				}
+				if (dirY > -0.1 && dirY < 0.1)
+				{
+					dirY = 0;
+				}
+				break;
+			}
+			case R_JOYSTICK:
+			{
+				dirX = storedAxes[3];
+				dirY = storedAxes[4];
+				if (dirX > -0.1 && dirX < 0.1)
+				{
+					dirX = 0;
+				}
+				if (dirY > -0.1 && dirY < 0.1)
+				{
+					dirY = 0;
+				}
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	return Vector3(dirX, dirY, 0);
 }

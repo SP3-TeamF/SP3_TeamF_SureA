8cdc0afdc2f4654b0bcdb18b2a2246ac4d349418
zhanweioh <zhanweioh@gmail.com>
2016-08-24 10:26:20 +0800

    invemt

---
+++ SP-3 Framework/Base/Source/Invent.h
@@ -0,0 +1,34 @@
+
+#ifndef INVENT_H
+#define INVENT_H
+
+
+
+
+#define Inventory Invents::GetInstance()
+
+class Invents
+{
+public:
+
+    Invents(){};
+    ~Invents(){
+      
+    };
+
+    static Invents& GetInstance()
+    {
+        static Invents inventv;
+        return inventv;
+    }
+
+ 
+    float firebullet = 50;
+	float airbullet = 50;
+	float waterbullet = 50;
+	float netbullet = 50;
+
+   
+};
+
+#endif INVENT_H
